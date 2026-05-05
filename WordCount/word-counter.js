9121f6b2dc8f9135f47516d0832fafa94d158345
DuyHuynh <dhuynh12@my.stlcc.edu>
2026-05-04 21:03:24 -0500

    Remove safety check for document existence

--- WordCount/word-counter.js
+++ WordCount/word-counter.js
@@ -1,25 +1,20 @@
-// Safety check to prevent CodeGrade's server from crashing
-if (typeof document !== 'undefined') {
-    
-    // Find the first paragraph
-    let firstParagraph = document.querySelector("p");
+// Find the first paragraph
+let firstParagraph = document.querySelector("p");
 
-    // Get the text and split it into an array of words
-    let text = firstParagraph.innerText;
-    let wordsArray = text.split(" ");
-    let wordCount = wordsArray.length;
+// Get the text and split it into an array of words
+let text = firstParagraph.innerText;
+let wordsArray = text.split(" ");
+let wordCount = wordsArray.length;
 
-    // Calculate reading time (assuming 225 words per minute)
-    let readingTime = Math.round(wordCount / 225);
+// Calculate reading time (assuming 225 words per minute)
+let readingTime = Math.round(wordCount / 225);
 
-    // Create the text string
-    let timeMessage = "Estimated reading time: " + readingTime + " minute(s).";
+// Create the text string
+let timeMessage = "Estimated reading time: " + readingTime + " minute(s).";
 
-    // Create a new <p> element and set its text
-    let newParagraph = document.createElement("p");
-    newParagraph.innerText = timeMessage;
+// Create a new <p> element and set its text
+let newParagraph = document.createElement("p");
+newParagraph.innerText = timeMessage;
 
-    // Insert the new paragraph before the first paragraph
-    firstParagraph.parentNode.insertBefore(newParagraph, firstParagraph);
-    
-}
+// Insert the new paragraph before the first paragraph
+firstParagraph.parentNode.insertBefore(newParagraph, firstParagraph);
